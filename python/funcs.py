7b732b29f2189ed914e872a64dd270829b128c17
RKCF <steinkean@gmail.com>
2017-02-20 17:02:37 -0600

    did problem 2

--- python/funcs.py
+++ python/funcs.py
@@ -9,3 +9,20 @@ def problem1():
         elif n % 5 == 0:
             l.append(n)
     return sum(l)
+
+def problem2():
+    """ By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms. """
+    fib = [1, 1]
+    while True:
+        new = fib[len(fib) - 1] + fib[len(fib) - 2]
+        if new > 4000000:
+            break
+        else:
+            fib.append(new)
+
+    s = 0
+    for n in fib:
+        if n % 2 == 0:
+            s += n
+
+    return s
