0311a3e850af32cba7effb4cb238b7cf6d88632d
RKCF <steinkean@gmail.com>
2017-02-21 20:55:40 -0600

    solved problem 4

--- python/funcs.py
+++ python/funcs.py
@@ -1,5 +1,22 @@
 """ Project Euler problems in python """
 
+NOT_PRIMES = []
+
+def is_prime(n, m):
+    """ Returns True if number is a prime """
+    if n in NOT_PRIMES:
+        return False
+    for x in range(2, int(n/2)):
+        if n % x == 0:
+            return False
+
+    for i in range(2, m):
+        if i > m/2:
+            break
+        NOT_PRIMES.append(i * n)
+
+    return True
+
 def problem1():
     """ Find the sum of all the multiples of 3 or 5 below 1000. """
     l = []
@@ -19,10 +36,32 @@ def problem2():
             break
         else:
             fib.append(new)
-
     s = 0
     for n in fib:
         if n % 2 == 0:
             s += n
 
     return s
+
+def problem3():
+    """ What is the largest prime factor of the number 600851475143 ? """
+    pfactors = []
+    target = 600851475143
+    for div in range(2, int(target/2)):
+        if target % div == 0:
+            if is_prime(div, target):
+                pfactors.append(div)
+    return max(pfactors)
+
+def problem4():
+    """Find the largest palindrome made from the product of two 3-digit numbers."""
+    prods = [x * y for x in range(100, 1000) for  y in range(100, 1000)]
+    palindromes = []
+    for n in prods:
+        reverse = []
+        normal = list(str(n))
+        for i in str(n):
+            reverse.insert(0, i)
+        if reverse == normal:
+            palindromes.append(n)
+    return max(palindromes)
